f6e09a83a88909de8e6d4e881671d4625e0edfa3
Mohamed Hegazi <m.hegazy94@hotmail.com>
2016-03-12 15:11:57 +0100

    Fixed responsiveness,font scale and overlapping

---
+++ WeatherApp/js/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var app = angular.module('weatherApp', ['ngMaterial']);
+
+app.controller('ListController', function($scope, $http) {
+  $scope.isSearching = false;
+  $scope.$watch("searchTerm", function() {
+    var resultsTop = angular.element(document.querySelector('#resultsTop')); //search for top result block
+    var resultsBottom = angular.element(document.querySelector('#resultsBottom')); //search for bottom result block
+    //enable progress bar if there is a delay
+
+    if ($scope.searchTerm != null && $scope.searchTerm != '') {
+      $scope.isSearching = true;
+      $http({
+        method: "GET",
+        url: "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.searchTerm + "&mode=json&units=metric&cnt=7&appid=3d6f22f902d1b2ef7bd82ae2f0672360"
+      }).then(function mySuccess(response) {
+        $scope.weatherData = response.data;
+        $scope.isSearching = false;
+      }, function myError(response) {
+        $scope.weatherData = "Something went wrong";
+        $scope.isSearching = false;
+      });
+
+
+    }
+  });
+});
+
+//theme settings
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('light-green', {
+      'default': '300'
+    })
+    .accentPalette('green')
+    .backgroundPalette('teal', {
+      'default': '50'
+    });
+});
