c115302622f754a7265bc6a04d5a87d96988ba28
Mohamed Hegazi <m.hegazy94@hotmail.com>
2016-03-14 00:27:13 +0100

    Added Current Weather

--- weather/js/app.js
+++ weather/js/app.js
@@ -13,14 +13,33 @@ app.controller('ListController', function($scope, $http) {
       $scope.isSearching = true;
       $http({
         method: "GET",
-        url: "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.searchTerm + "&mode=json&units=metric&cnt=7&appid=3d6f22f902d1b2ef7bd82ae2f0672360"
-      }).then(function mySuccess(response) {
-        $scope.weatherData = response.data;
+        url: "http://api.openweathermap.org/data/2.5/weather?q=" + $scope.searchTerm + "&mode=json&units=metric&appid=3d6f22f902d1b2ef7bd82ae2f0672360"
+      }).then(function mySuccess(current) {
+        $scope.currentWeatherData = current.data;
         $scope.isSearching = false;
-      }, function myError(response) {
-        $scope.weatherData = "Something went wrong";
+
+
+
+        $http({
+          method: "GET",
+          url: "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.searchTerm + "&mode=json&units=metric&cnt=7&appid=3d6f22f902d1b2ef7bd82ae2f0672360"
+        }).then(function mySuccess(response) {
+          $scope.weatherData = response.data;
+          $scope.isSearching = false;
+        }, function myError(response) {
+          $scope.weatherData = "Something went wrong";
+          $scope.isSearching = false;
+        });
+
+
+
+
+      }, function myError(current) {
+        $scope.currentWeatherData = "Something went wrong";
         $scope.isSearching = false;
       });
+
+
 
 
     }
