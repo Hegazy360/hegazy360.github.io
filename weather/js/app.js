bfcdcdf3f8d1db52808fc17dc3ab1631aa058071
Mohamed Hegazi <m.hegazy94@hotmail.com>
2016-03-14 16:33:34 +0100

    Fixed results not appearing sometimes

--- weather/js/app.js
+++ weather/js/app.js
@@ -18,8 +18,6 @@ app.controller('ListController', function($scope, $http) {
         $scope.currentWeatherData = current.data;
         $scope.isSearching = false;
 
-
-
         $http({
           method: "GET",
           url: "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.searchTerm + "&mode=json&units=metric&cnt=7&appid=3d6f22f902d1b2ef7bd82ae2f0672360"
@@ -27,21 +25,54 @@ app.controller('ListController', function($scope, $http) {
           $scope.weatherData = response.data;
           $scope.isSearching = false;
         }, function myError(response) {
-          $scope.weatherData = "Something went wrong";
-          $scope.isSearching = false;
+
+          $http({
+            method: "GET",
+            url: "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.searchTerm + "&mode=json&units=metric&cnt=7&appid=3d6f22f902d1b2ef7bd82ae2f0672360"
+          }).then(function mySuccess(response) {
+            $scope.weatherData = response.data;
+            $scope.isSearching = false;
+          }, function myError(response) {
+            $scope.weatherData = "Something went wrong";
+            $scope.isSearching = false;
+          });
         });
 
 
-
-
       }, function myError(current) {
-        $scope.currentWeatherData = "Something went wrong";
-        $scope.isSearching = false;
+        $http({
+          method: "GET",
+          url: "http://api.openweathermap.org/data/2.5/weather?q=" + $scope.searchTerm + "&mode=json&units=metric&appid=3d6f22f902d1b2ef7bd82ae2f0672360"
+        }).then(function mySuccess(current) {
+          $scope.currentWeatherData = current.data;
+          $scope.isSearching = false;
+
+          $http({
+            method: "GET",
+            url: "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.searchTerm + "&mode=json&units=metric&cnt=7&appid=3d6f22f902d1b2ef7bd82ae2f0672360"
+          }).then(function mySuccess(response) {
+            $scope.weatherData = response.data;
+            $scope.isSearching = false;
+          }, function myError(response) {
+
+            $http({
+              method: "GET",
+              url: "http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.searchTerm + "&mode=json&units=metric&cnt=7&appid=3d6f22f902d1b2ef7bd82ae2f0672360"
+            }).then(function mySuccess(response) {
+              $scope.weatherData = response.data;
+              $scope.isSearching = false;
+            }, function myError(response) {
+              $scope.weatherData = "Something went wrong";
+              $scope.isSearching = false;
+            });
+          });
+
+
+        }, function myError(current) {
+          $scope.currentWeatherData = "Something went wrong";
+          $scope.isSearching = false;
+        });
       });
-
-
-
-
     }
   });
 });
